542026cafb10400a6bab77d35e2c583783abb2b8
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2014-07-13 17:51:32 +0200

    write.tgrass now writes init.txt

--- R/tgrass.R
+++ R/tgrass.R
@@ -8,7 +8,8 @@ read.tgrass = function(fname, localName = TRUE, useTempDir = TRUE, isGeoTiff = T
 	untar(fname, exdir = dir)
 	# start reading:
 	tab = read.table(file.path(dir, "list.txt"), sep = "|", as.is=TRUE)
-	crs = paste(read.table(file.path(dir, "proj.txt"), as.is=TRUE)[[1]], collapse=" ")
+	crs = paste(read.table(file.path(dir, "proj.txt"), as.is=TRUE)[[1]], 
+		collapse=" ")
 	r = raster::stack(file.path(dir, paste(tab[[1]], ".tif", sep="")))
 	proj4string(r) = CRS(crs)
 	# get start time:
@@ -19,7 +20,7 @@ read.tgrass = function(fname, localName = TRUE, useTempDir = TRUE, isGeoTiff = T
 	r
 }
 
-write.tgrass = function(obj, fname) {
+write.tgrass = function(obj, fname, ...) {
 	require(raster)
 	if (is(obj, "STFDF")) {
 		end.time = obj@endTime
@@ -32,15 +33,30 @@ write.tgrass = function(obj, fname) {
 	setwd(dir)
 	# write .tifs:
 	for (i in 1:nlayers(obj))
-		writeRaster(raster(obj, layer=i), n[i])
+		writeRaster(raster(obj, layer=i), n[i], ...)
 	# write proj.txt:
-	write.table(data.frame(x = proj4string(obj)), "proj.txt", col.names=FALSE, quote = FALSE, row.names = FALSE)
+	write.table(data.frame(x = proj4string(obj)), "proj.txt", col.names=FALSE, 
+		quote = FALSE, row.names = FALSE)
 	# write list.txt:
 	start.time = getZ(obj)
 	if (is.null(end.time))
 		end.time = start.time
 	tab = data.frame(names(obj), start.time, end.time)
-	write.table(tab, "list.txt", sep = "|", col.names = FALSE, quote = FALSE, row.names = FALSE)
+	write.table(tab, "list.txt", sep = "|", col.names = FALSE, 
+		quote = FALSE, row.names = FALSE)
+	# write init.txt:
+	f = file("init.txt", "w")
+	cat("stds_type=strds\n", file = f)
+	cat("format=GTiff\n", file = f)
+	cat("temporal_type=absolute\n", file = f)
+	cat("semantic_type=mean\n", file = f)
+	cat("number_of_maps=", nlayers(obj), "\n", file = f, sep="")
+	e = extent(obj)
+	cat("north=", e@ymax, "\n", file = f, sep="")
+	cat("south=", e@ymin, "\n", file = f, sep="")
+	cat("east=", e@xmax, "\n", file = f, sep="")
+	cat("west=", e@xmin, "\n", file = f, sep="")
+	close(f)
 	# tar.gz the thing:
 	#tar(file.path(olddir, fname), compression = "gzip")
 	cmd = paste("tar zcf", file.path(olddir, fname), ".")
