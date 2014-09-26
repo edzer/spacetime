0ed91ac92db69dc64cecaa9ecf08efbb2308db45
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2014-09-26 21:44:00 +0200

    add parens

--- R/stinteraction.R
+++ R/stinteraction.R
@@ -1,6 +1,6 @@
 stInteraction <- function(x) { 
 	stopifnot(is(x, "STFDF"))
-	for (i in 1:dim(x)[3]) {
+	for (i in 1:(dim(x)[3])) {
 		y = as(x[,,i], "xts")
 		x[[i]] = as.vector(t(y - (rowMeans(y) %o% colMeans(y))/mean(y)))
 	}
